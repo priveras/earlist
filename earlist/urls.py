9bd06b27d0edeaec45c8af9bd74c7079f484aaaa
Pablo Rivera <priveras@gmail.com>
2016-05-15 20:15:22 -0500

    Sitemap

--- earlist/urls.py
+++ earlist/urls.py
@@ -19,6 +19,7 @@ from blog import views
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
 
 urlpatterns = [
     url(r'^cuentas/perfil/votes/(?P<slug>[^\.]+)/vote/(?P<direction>[^\.]+)/$', views.vote, name='vote'),
