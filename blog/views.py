9b3352e52bbae7c064243a592aae80b82e700fcc
Pablo Rivera <priveras@gmail.com>
2016-06-07 23:46:02 -0500

    Update

--- blog/views.py
+++ blog/views.py
@@ -31,7 +31,7 @@ meta = Meta(
         url = "http://earlist.club/",
         title = 'Earlist',
         description = 'Earlist es el lugar para descubrir los mejores startups de tecnología en México. Únete a nuestra comunidad de apasionados por la innovación y la tecnología.',
-        image = 'http://postimg.org/image/68e46t3m9/',
+        image = 'https://scontent-dfw1-1.xx.fbcdn.net/t31.0-0/p480x480/13422280_1148244848559260_2213529633127398366_o.png',
         )
 
 class AboutView(generic.TemplateView):
