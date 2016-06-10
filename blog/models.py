3271fc6033c191d23884744731ca4e8eb72b5844
Pablo Rivera <priveras@gmail.com>
2016-06-10 10:10:22 -0500

    Voters

--- blog/models.py
+++ blog/models.py
@@ -34,6 +34,9 @@ class Voter(models.Model):
     created_at = models.DateTimeField(db_index=True, auto_now_add=True)
     updated_at = models.DateTimeField(db_index=True, auto_now_add=True)
 
+    def __str__(self):
+		return self.user
+
 
 class Event(models.Model):
 	user = models.ForeignKey(User)
