6c212706a5c160e74bd6b164722913904c8ec483
Pablo Rivera <priveras@gmail.com>
2017-06-27 20:41:30 -0400

    signup update

--- earlist/settings.py
+++ earlist/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
    
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['earlist.xyz']
 
 
 # Application definition
