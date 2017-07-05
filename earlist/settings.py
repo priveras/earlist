a40a6c94786fdf18d1f7b9548afcae2c0b476f9e
Pablo Rivera <priveras@gmail.com>
2017-07-05 13:01:02 -0500

    email backend

--- earlist/settings.py
+++ earlist/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
    
-ALLOWED_HOSTS = ['67.205.191.247']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
