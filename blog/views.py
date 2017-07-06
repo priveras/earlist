b7087688947a7d72218e19e6b4c048b7483443b0
Pablo Rivera <priveras@gmail.com>
2017-07-06 09:43:34 -0500

    update

--- blog/views.py
+++ blog/views.py
@@ -275,7 +275,7 @@ class DetailView(MetadataMixin, generic.DetailView):
             use_title_tag = True,
             twitter_card = 'summary_large_image',
             url = "http://earlist.xyz/producto/" + self.object.slug + '/',
-            title = self.object.title + ' | Earlist',
+            title = 'Earlist |' + self.object.title,
             description = self.object.body,
             image = self.object.image_file.url,
             )
